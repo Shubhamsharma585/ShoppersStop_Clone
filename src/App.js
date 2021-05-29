75b9e2bd61b93713a41f811b941c9644de3119c3
manish7377 <manish7377873265@gmail.com>
2021-05-29 11:03:39 +0530

    carousel component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import React from "react"
 import NavBar from './Components/Navbar/NavBar';
 import NavbarList from './Components/Navbar/NavbarList';
+import Carousel from './Components/Carousel/Carousel';
+
+const images=[
+  "https://sslimages.shoppersstop.com/sys-master/root/haa/h58/16769223950366/Beauty-HP-Carousal-Arcelia2-Web.jpg",
+  "https://sslimages.shoppersstop.com/sys-master/root/h42/hef/16769225130014/Ode-to-Beauty-Web.jpg",
+  "https://sslimages.shoppersstop.com/sys-master/root/hda/h2c/16775910129694/Beauty-HP-Carousals-Day-2-Fragrances-Web.jpg",
+  "https://sslimages.shoppersstop.com/sys-master/root/h42/hef/16769225130014/Ode-to-Beauty-Web.jpg"
+]
+
 function App() {
   return (
     <div className="App">
       <NavBar />
       <NavbarList />
+      <Carousel images={images}/>
     </div>
   );
 }
